024d3100e0437be0416bbf8a13f2c40141b2a0c0
SudhangsuShekharBairagi <bairagisudhangsu02@gmail.com>
2024-11-28 20:18:52 +0530

    modified JavaScript2

--- script.js
+++ script.js
@@ -1,195 +1,138 @@
 
-let box1 = document.querySelectorAll(".mybutton");
+let box1 = Array.from(document.querySelectorAll(".mybutton"));
 let reset = document.querySelector("#mybutton1");
 let re = document.querySelector("#hi");
 let hide1 = document.querySelector(".msg_container");
 let new_g = document.querySelector("#new_g");
 
-let playerx = 'Computer';
-let playery = 'User';
+let playerX = "Computer";
+let playerY = "User";
+let isUserTurn = true; // Tracks turn (true = User, false = Computer)
+let count = 0; // Counts moves
 
-let count = 0;
-let true0 = true;
-
-const winpattan = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6]             // Diagonals
 ];
 
-
-// function for New Game & Reset button 
-const reset1 = () => {
-    true0 = true;
-    aval();
-    hide1.classList.add("hide");
-    count = 0;
-}
-
-// user click function
-box1.forEach((box) => {
-    box.addEventListener("click", () => {
-        if (true0) {
-            box.innerText = "0";
-            true0 = false;
-            box.disabled = true;
-        }
-
-
-        if (!checkWin()) {
-
-            computerPlay();
-        }
-
-
-    });
-
-});
-
-// computer play function
-const computerPlay = () => {
-
-    let posBlank = 0;
-    const array = [];
-
-    box1.forEach((box) => {
-        const blank = box.innerText === "";
-
-        if (blank === true) {
-            posBlank++;
-            array.push(box.value);
-        }
-
-    });
-
-    // const randomIndex = Math.floor(Math.random() * array.length);
-    // const randomElement = array[randomIndex];
-    // // computer move
-    // box1.forEach((box) => {
-    //     if (true0 === false) {
-    //         document.getElementById(`${randomElement}`).innerText = "X";
-
-    //         true0 = true;
-    //         document.getElementById(`${randomElement}`).disabled = true;
-
-    //     }
-    // });
-
-    // check the win patttern for computer next move
-    for (let pattern of winpattan) {
-        let pos1 = box1[pattern[0]].innerText;
-        let pos2 = box1[pattern[1]].innerText;
-        let pos3 = box1[pattern[2]].innerText;
-        //   console.log(pattern[0]);
-
-
-        if ((pos1 != "" && pos2 != "" && pos3 == "") || (pos1 == "" && pos2 != "" && pos3 != "") || (pos1 != "" && pos2 == "" && pos3 != "")) {
-            if (pos1 === pos2 && pos3 == "") {
-                randomElement = box1[pattern[2]].value;
-                computerPlay2(randomElement);
-            } else if (pos1 === pos3 && pos2 == "") {
-                randomElement = box1[pattern[1]].value;
-                computerPlay2(randomElement);
-            } else if (pos2 === pos3 && pos1 == "") {
-                randomElement = box1[pattern[0]].value;
-                computerPlay2(randomElement);
-            } else {
-                comPlay = true;
-            }
-        } else {
-            comPlay = true;
-        }
-    }
-
-    if (comPlay) {
-        randomIndex = Math.floor(Math.random() * array.length);
-        // console.log(array.length);
-        comPlay = false;
-        randomElement = array[randomIndex];
-        computerPlay2(randomElement);
-
-    }
-
-}
-// function for computer move in the box
-const computerPlay2 = (randomElement) => {
-    // const randomElement2 = randomElement;
-    box1.forEach((box) => {
-        if (true0 === false) {
-            document.getElementById(`${randomElement}`).innerText = "X";
-            // box.innerText = "X";
-            true0 = true;
-            document.getElementById(`${randomElement}`).disabled = true;
-
-        }
-    });
-    checkWin(); //calling the checkWin function for the the win pattern
-}
-
-// function for disable the all button box 
-const dis = () => {
-    for (let box of box1) {
-        box.disabled = true;
-    }
-}
-// able the all button box 
-const aval = () => {
-    for (let box of box1) {
-        box.disabled = false;
+// Reset Function: Clears board and state
+const resetGame = () => {
+    box1.forEach(box => {
         box.innerText = "";
-    }
-}
-// function for the winner 
-const winner = (winner) => {
-    var x = "X";
-    var y = "0";
-
-    if (winner == 0) {
-        re.innerText = "Congratulation, Winner is " + playery;
-
-    } else if (winner == x) {
-        re.innerText = "Congratulation, Winner is " + playerx;
-    } else {
-        re.innerText = "Match Draw";
-    }
-
-
-    hide1.classList.remove("hide");
-    dis();
-}
-// function for check the winner
-const checkWin = () => {
+        box.disabled = false;
+    });
+    hide1.classList.add("hide");
+    re.innerText = "";
+    isUserTurn = true;
     count = 0;
-    for (let pattern of winpattan) {
-        let pos1 = box1[pattern[0]].innerText;
-        let pos2 = box1[pattern[1]].innerText;
-        let pos3 = box1[pattern[2]].innerText;
+};
 
+// Check for Winner or Draw
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let [a, b, c] = pattern;
+        let values = [box1[a].innerText, box1[b].innerText, box1[c].innerText];
 
-
-        if (pos1 != "" && pos2 != "" && pos3 != "") {
-            if (pos1 === pos2 && pos2 === pos3) {
-                winner(pos1);
-                return true;
-            } else {
-                count++;
-            }
+        if (values[0] && values[0] === values[1] && values[1] === values[2]) {
+            declareWinner(values[0]); // "X" or "0"
+            return true;
         }
     }
-    // check if the match is draw
-    if (count >= 8) {
-        let pos = 9;
-        winner(pos);
+
+    // Check Draw Condition
+    if (box1.every(box => box.innerText !== "")) {
+        declareWinner(null); // Draw
         return true;
     }
     return false;
-}
+};
 
-new_g.addEventListener("click", reset1); // Event Listener for new game button
-reset.addEventListener("click", reset1); // Event Listener for reset button
+// Declare Winner or Draw
+const declareWinner = (winner) => {
+    if (winner === "X") {
+        re.innerText = `Congratulations! Winner is ${playerX}`;
+    } else if (winner === "0") {
+        re.innerText = `Congratulations! Winner is ${playerY}`;
+    } else {
+        re.innerText = "It's a Draw!";
+    }
+    hide1.classList.remove("hide");
+    box1.forEach(box => (box.disabled = true)); // Disable all boxes
+};
 
+// User Play
+box1.forEach((box, index) => {
+    box.addEventListener("click", () => {
+        if (isUserTurn && box.innerText === "") {
+            box.innerText = "0";
+            box.disabled = true;
+            count++;
+            isUserTurn = false;
+
+            if (!checkWinner()) {
+                setTimeout(computerPlay, 300); // Allow computer to play after a slight delay
+            }
+        }
+    });
+});
+
+// Computer Play
+const computerPlay = () => {
+    // Priority: Winning Move > Blocking Move > Center > Random Move
+    let moveMade = false;
+
+    // Step 1: Check for Winning Move
+    for (let pattern of winPatterns) {
+        let [a, b, c] = pattern;
+        let values = [box1[a].innerText, box1[b].innerText, box1[c].innerText];
+
+        if (values.filter(v => v === "X").length === 2 && values.includes("")) {
+            makeMove([a, b, c].find(idx => box1[idx].innerText === ""), "X");
+            moveMade = true;
+            break;
+        }
+    }
+
+    // Step 2: Block User's Winning Move
+    if (!moveMade) {
+        for (let pattern of winPatterns) {
+            let [a, b, c] = pattern;
+            let values = [box1[a].innerText, box1[b].innerText, box1[c].innerText];
+
+            if (values.filter(v => v === "0").length === 2 && values.includes("")) {
+                makeMove([a, b, c].find(idx => box1[idx].innerText === ""), "X");
+                moveMade = true;
+                break;
+            }
+        }
+    }
+
+    // Step 3: Take Center if Available
+    if (!moveMade && box1[4].innerText === "") {
+        makeMove(4, "X");
+        moveMade = true;
+    }
+
+    // Step 4: Make a Random Move
+    if (!moveMade) {
+        let emptyBoxes = box1.filter(box => box.innerText === "");
+        if (emptyBoxes.length > 0) {
+            let randomBox = emptyBoxes[Math.floor(Math.random() * emptyBoxes.length)];
+            makeMove(box1.indexOf(randomBox), "X");
+        }
+    }
+
+    if (!checkWinner()) isUserTurn = true; // Switch turn to User
+};
+
+// Make a Move
+const makeMove = (index, symbol) => {
+    box1[index].innerText = symbol;
+    box1[index].disabled = true;
+    count++;
+};
+
+// Event Listeners for Reset and New Game
+reset.addEventListener("click", resetGame);
+new_g.addEventListener("click", resetGame);
